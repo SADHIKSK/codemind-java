673222cf3b6057f5c7a93c0864741acbe69ea7fe
Saadhik shaik <96807695+SADHIKSK@users.noreply.github.com>
2023-05-31 14:47:32 +0530

    import java.util.*; public class Code{     public static void main(String args[]){         Scanner sc = new Scanner(System.in);         int n = sc.nextInt();         int c = 0;         if(n%3==0){             System.out.format("Pling");             c++;         }         if(n%5==0){             System.out.format("Plang");             c++;         }         if(n%7==0){             System.out.format("Plong");             c++;         }         if(c==0){             System.out.format("%d",n);         }     } }

---
+++ RainDrop.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Code{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int c = 0;
+        if(n%3==0){
+            System.out.format("Pling");
+            c++;
+        }
+        if(n%5==0){
+            System.out.format("Plang");
+            c++;
+        }
+        if(n%7==0){
+            System.out.format("Plong");
+            c++;
+        }
+        if(c==0){
+            System.out.format("%d",n);
+        }
+    }
+}
