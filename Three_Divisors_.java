5160627791a73b54209038e8f61893f2fe6981de
Saadhik shaik <96807695+SADHIKSK@users.noreply.github.com>
2023-05-31 14:53:03 +0530

    import java.util.*; public class sadhik {     public static boolean isPrime(int n)     {         for(int i=2;i<=(int)Math.sqrt(n);i++)         {             if(n%i==0)               return false;         }         return true;     }     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int m,count=0;         m=sc.nextInt();         for(int i=2;i<=(int)Math.sqrt(m);i++)         {               if(isPrime(i))                  count++;         }         System.out.println(count);     } }

---
+++ Three_Divisors_.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class sadhik
+{
+    public static boolean isPrime(int n)
+    {
+        for(int i=2;i<=(int)Math.sqrt(n);i++)
+        {
+            if(n%i==0)
+              return false;
+        }
+        return true;
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int m,count=0;
+        m=sc.nextInt();
+        for(int i=2;i<=(int)Math.sqrt(m);i++)
+        {
+              if(isPrime(i))
+                 count++;
+        }
+        System.out.println(count);
+    }
+}
