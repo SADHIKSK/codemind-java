ed1cf7d3efa0515adabbd209aea115bd8c30c249
Saadhik shaik <96807695+SADHIKSK@users.noreply.github.com>
2023-05-31 14:43:22 +0530

    import java.util.*; public class Code{     public static void main(String args[]){         Scanner sc = new Scanner(System.in);         int n = sc.nextInt();         if(n==4 || n==5 || n==6){             System.out.format("Summer");         }         else if(n==7 || n==8 || n==9 || n==10){             System.out.format("Rainy");         }         else if(n==11 || n==12 || n==1){             System.out.format("Winter");         }         else if(n==2 || n==3){             System.out.format("Spring");         }         else{             System.out.format("-1");         }     } }

---
+++ Display_Season.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Code{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n==4 || n==5 || n==6){
+            System.out.format("Summer");
+        }
+        else if(n==7 || n==8 || n==9 || n==10){
+            System.out.format("Rainy");
+        }
+        else if(n==11 || n==12 || n==1){
+            System.out.format("Winter");
+        }
+        else if(n==2 || n==3){
+            System.out.format("Spring");
+        }
+        else{
+            System.out.format("-1");
+        }
+    }
+}
